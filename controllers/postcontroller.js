9815b110fb7a4ec40dc1867a85e70396b6e7e6b1
Gabrielle Ford <gabrielledeva@gmail.com>
2021-11-29 16:17:32 -0500

    updated endpoints, post model, and validatejwt to save username

--- controllers/postcontroller.js
+++ controllers/postcontroller.js
@@ -12,6 +12,7 @@ router.get('/test', validateJWT, (req, res) => {
 router.post('/create', validateJWT, async (req, res) => {
     const { private, title, image, description, tag } = req.body.post;
     const id  = req.user_id;
+    const username = req.username;
     console.log(`Owner ID: ${id}`);
 
     const post = {
@@ -20,7 +21,8 @@ router.post('/create', validateJWT, async (req, res) => {
         image,
         description,
         tag,
-        owner_id: id
+        owner_id: id,
+        username: username
     }
 
     try {
@@ -54,6 +56,22 @@ router.get('/', async (req, res) => {
     }
 });
 
+// * Get all posts when logged in *
+// http://localhost:3000/post/posts
+router.get('/posts', async (req, res) => {
+    const id = req.user_id;
+
+    try {
+        const allPosts = await Post.findAll();
+
+        res.status(200).json(allPosts);
+    } catch (err) {
+        res.status(500).json({
+            error: `Error: ${err}`
+        });
+    }
+});
+
 // * Get all user posts *
 // http://localhost:3000/post/myposts
 router.get('/myposts', validateJWT, async (req, res) => {
@@ -75,15 +93,15 @@ router.get('/myposts', validateJWT, async (req, res) => {
 });
 
 // * Get other users' public posts
-// http://localhost:3000/post/posts/:userid
-router.get('/posts/:userid', async (req, res) => {
-    const id = req.params.userid;
+// http://localhost:3000/post/posts/:username
+router.get('/posts/:username', async (req, res) => {
+    const username = req.params.username;
 
     try {
         const userPosts = await Post.findAll({
             where: {
                 private: false,
-                owner_id: id
+                username: username
             }
         });
 
@@ -95,11 +113,30 @@ router.get('/posts/:userid', async (req, res) => {
     }
 });
 
+// * Get all of another user's posts *
+// http://localhost:3000/post/posts/all/:username
+router.get('/posts/all/:username', validateJWT, async (req, res) => {
+    const username = req.params.username;
+
+    try {
+        const userPosts = await Post.findAll({
+            where: {
+                username: username
+            }
+        })
+
+        res.status(200).json(userPosts);
+    } catch (err) {
+        res.status(500).json({
+            err: `Error: ${err}`
+        })
+    }
+})
+
 // * Get post by id *
 // http://localhost:3000/post/:id
 router.get('/:id', validateJWT, async (req, res) => {
     const postId = req.params.id;
-    const id = req.user_id;
 
     try {
         const postById = await Post.findAll({
@@ -113,6 +150,47 @@ router.get('/:id', validateJWT, async (req, res) => {
         res.status(500).json({ error: `Error: ${err}`});
     }
 });
+
+// * Get posts by tag *
+//http://localhost:3000/post/tag/:tag
+router.get('/tag/:tag', async (req, res) => {
+    const tag = req.params.tag;
+
+    try {
+        const postsByTag = await Post.findAll({
+            where: {
+                private: false,
+                tag: tag
+            }
+        });
+
+        res.status(200).json(postsByTag);
+    } catch (err) {
+        res.status(500).json({
+            err: `Error: ${err}`
+        });
+    }
+});
+
+// * Get public & private posts by tag *
+// http://localhost:3000/post/tag/all/:tag
+router.get('/tag/all/:tag', validateJWT, async (req, res) => {
+    const tag = req.params.tag;
+
+    try {
+        const postsByTag = await Post.findAll({
+            where: {
+                tag: tag
+            }
+        });
+
+        res.status(200).json(postsByTag);
+    } catch (err) {
+        res.status(500).json({
+            err: `Error: ${err}`
+        });
+    }
+})
 
 // * Update post *
 // http://localhost:3000/edit/:id
