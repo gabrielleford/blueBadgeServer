ffe20f6e16ef5a70f4d38947c7170b2d32433587
Gabrielle Ford <gabrielledeva@gmail.com>
2021-12-04 12:05:01 -0500

    commented out more code

--- db.js
+++ db.js
@@ -1,8 +1,9 @@
 const { Sequelize } = require("sequelize/dist");
 
 const db = new Sequelize(
-  process.env.DATABASE_URL ||
-  `postgresql://postgres:${encodeURIComponent(process.env.PASS)}@localhost/instapet`,
+  process.env.DATABASE_URL,
+  //  ||
+  // `postgresql://postgres:${encodeURIComponent(process.env.PASS)}@localhost/instapet`,
 //   process.env.HOST == 'local' ? 
 // {
 //   dialect: 'postgres'
